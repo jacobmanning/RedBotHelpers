fcec32aa0b8f796d2fbb20dc9cda4b8c40953872
Jacob Manning <jacobmanning@pitt.edu>
2019-03-27 23:11:40 -0400

    Cleanup experimental turns example

--- examples/turns/turns.ino
+++ examples/turns/turns.ino
@@ -2,23 +2,6 @@
 
 auto gonk = RedBot();
 
-void test_pivot(const int angle)
-{
-  // Explicit types
-  redbot::experimental::pivot_right(gonk, Angle(angle));
-  redbot::experimental::pivot_left(gonk, Angle(angle));
-}
-
-void test_pivot_literal()
-{
-  // Literal operators
-  redbot::experimental::pivot_right(gonk, 90_degrees);
-  redbot::experimental::pivot_left(gonk, 90_degrees);
-
-  redbot::experimental::pivot_right(gonk, 90_deg);
-  redbot::experimental::pivot_left(gonk, 90_deg);
-}
-
 void setup()
 {
   Serial.begin(9600);
@@ -26,7 +9,14 @@ void setup()
 
 void loop()
 {
-  test_pivot(90);
-  // test_pivot(180);
-  // test_pivot(270);
+  // Explicit types
+  redbot::experimental::pivot_right(gonk, Angle(90));
+  redbot::experimental::pivot_left(gonk, Angle(90));
+
+  // Literal operators
+  redbot::experimental::pivot_right(gonk, 90_degrees);
+  redbot::experimental::pivot_left(gonk, 90_degrees);
+
+  redbot::experimental::pivot_right(gonk, 90_deg);
+  redbot::experimental::pivot_left(gonk, 90_deg);
 }
